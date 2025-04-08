1bd4f1c8c22197643584b1b7399e1408df8ba0a4
Robert Navarro <robertlorenzonavarrovalle2@gmail.com>
2025-04-08 16:00:53 -0400

    1er commit, Para el diseño se usara MATERIAL UI, ya esta un ejemplo de rutas,

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { TextField, Typography } from '@mui/material'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div> 
+        <Typography variant='h3' >Home </Typography> 
+
+
+        </div>
+  )
+}
